d0d694d7f86eef98f37cbfc2c510e76b3c8c74b2
Konstantin Korovin <cosmo.stan@yandex.com>
2025-10-17 20:29:39 +0300

    feat: add user ORM model

---
+++ src/models/User.ts
@@ -0,0 +1,50 @@
+import type { JSONSchema } from 'objection';
+import { Model } from '../database';
+import { encrypt } from '@/lib/secure';
+
+export class User extends Model {
+  id!: number;
+
+  fullName!: string;
+
+  birthDate!: Date | null;
+
+  email!: string;
+
+  passwordDigest!: string;
+
+  role!: 'user' | 'admin';
+
+  isActive!: boolean;
+
+  createdAt!: Date;
+
+  static get tableName() {
+    return 'users';
+  }
+
+  static get jsonSchema(): JSONSchema {
+    return {
+      type: 'object',
+      required: ['fullName', 'email', 'password'],
+      properties: {
+        id: { type: 'integer' },
+        fullName: { type: 'string', minLength: 3, maxLength: 255 },
+        birthDate: { type: ['string', 'null'], format: 'date-time' },
+        email: { type: 'string', format: 'email', maxLength: 255 },
+        password: { type: 'string', minLength: 5, maxLength: 255 },
+        role: { type: 'string', enum: ['user', 'admin'] },
+        isActive: { type: 'boolean' },
+        createdAt: { type: 'string', format: 'date-time' },
+      },
+    };
+  }
+
+  set password(password: string) {
+    this.passwordDigest = encrypt(password);
+  }
+
+  verifyPassword(password: string) {
+    return encrypt(password) === this.passwordDigest;
+  }
+}
