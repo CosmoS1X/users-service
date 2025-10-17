a7924e8b715004fb4ec04ae3b5abbb9aaabb39d2
Konstantin Korovin <cosmo.stan@yandex.com>
2025-10-17 19:30:17 +0300

    feat: initialize new express app with logging

--- bin/index.ts
+++ bin/index.ts
@@ -1,3 +1,16 @@
+import 'dotenv/config';
+import debug from 'debug';
 import app from '../src';
 
-app();
+const port = process.env.PORT || 5000;
+const log = debug('server:log');
+const errorLog = debug('server:error');
+
+app
+  .listen(port, () => {
+    log(`✅ Server is running on http://localhost:${port}`);
+  })
+  .on('error', (error) => {
+    errorLog('❌ Server error:', error.message);
+    process.exit(1);
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export default ():void => {
-  console.log('Hello from new TypeScript repository!');
-};
+import express from 'express';
+import { morganConfig } from './configs';
+
+const app = express();
+
+app.use(morganConfig);
+app.use(express.json());
+
+export default app;
