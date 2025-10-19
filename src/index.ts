642d8157750347d610225ede9f00141333ff9ede
Konstantin Korovin <cosmo.stan@yandex.com>
2025-10-19 20:12:27 +0300

    feat: connect routers to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { morganConfig } from './configs';
+import routers from './routes';
 import { errorHandler } from './middlewares';
 
 const app = express();
 
 app.use(morganConfig);
 app.use(express.json());
+app.use('/api', ...routers);
 app.use(errorHandler);
 
 export default app;
