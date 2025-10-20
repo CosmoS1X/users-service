b5a4962eaf1daacb6bc89e707b087da96ed658d7
Konstantin Korovin <cosmo.stan@yandex.com>
2025-10-20 14:26:02 +0300

    feat: add sessions with passport to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-import { morganConfig } from './configs';
+import { morganConfig, sessionConfig } from './configs';
+import passport from './lib/passport';
 import routers from './routes';
 import { errorHandler } from './middlewares';
 
 const app = express();
 
 app.use(morganConfig);
+app.use(sessionConfig);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', ...routers);
