5f96e9af9878a91f0be780ed4d04b84cb2318ec6
Konstantin Korovin <cosmo.stan@yandex.com>
2025-10-21 16:50:35 +0300

    test: add endpoints enum for testing

---
+++ __tests__/endpoints.ts
@@ -0,0 +1,9 @@
+enum Endpoints {
+  Login = '/api/login',
+  Logout = '/api/logout',
+  CheckAuth = '/api/check-auth',
+  Users = '/api/users',
+  User = '/api/users/:id',
+}
+
+export default Endpoints;
